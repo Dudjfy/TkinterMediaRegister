193d5e7ef37cc765be8be6b1dccdd134588694c5
Dmitry Chirin <chirin.dima@gmail.com>
2021-12-14 12:52:09 +0100

    made it possible to add items to list using add buttons and inputs in the entry fields, added class for items data and class for enum states

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from tkinter import *
 from tkinter import filedialog, ttk
 from PIL import ImageTk, Image
@@ -6,6 +7,68 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title("Media Register")
 root.geometry("470x330")
+
+lst_box = Listbox(root, height=14, width=35, bd=2, relief=GROOVE)
+lst_box.place(x=245, y=35)
+
+
+class ItemStates(Enum):
+    BOOKS = 0
+    MOVIES = 1
+    ALL = 2
+
+
+def update_lst_box(items_data):
+    lst_box.delete(0, END)
+    for item in items_data:
+        lst_box.insert(END, item)
+
+
+class ItemsData:
+    def __init__(self):
+        self.books = []
+        self.movies = []
+        self.all = []
+
+        self.state = ItemStates.ALL
+
+    def add_book(self, title, author, pages):
+        if len(title.strip()) > 1 and len(author.strip()) and pages > 0:
+            book = f"{title} ({author}, {pages} pages)"
+            self.books.append(book)
+            self.all.append(book)
+            update_lst_box(self.get_items())
+
+    def add_movie(self, title, director, length):
+        if len(title.strip()) > 1 and len(director.strip()) and length > 0:
+            movie = f"{title} ({director}, {length} minutes)"
+            self.movies.append(movie)
+            self.all.append(movie)
+            update_lst_box(self.get_items())
+
+    def change_state(self, state):
+        self.state = state
+
+    def get_items(self):
+        if self.state == ItemStates.BOOKS:
+            return self.movies
+        elif self.state == ItemStates.MOVIES:
+            return self.books
+        else:
+            return self.all
+
+
+items_data = ItemsData()
+
+# def insert_book(title, author, pages):
+#     if len(title.strip()) > 1 and len(author.strip()) and pages > 0:
+#         lst_box.insert(END, f"{title} ({author}, {pages} pages)")
+#
+#
+# def insert_movie(title, director, length):
+#     if len(title.strip()) > 1 and len(director.strip()) and length > 0:
+#         lst_box.insert(END, f"{title} ({director}, {length} minutes)")
+
 
 notebook = ttk.Notebook(root)
 notebook.place(x=15, y=15)
@@ -27,32 +90,43 @@ positions = [
     (100, 170),
 ]
 
+book_title = StringVar()
+book_author = StringVar()
+book_pages = IntVar()
+
 book_widgets = {
     "books_title_lbl": Label(books, text="Title:", bg="white"),
-    "books_title_e": Entry(books, width=16, bd=2, relief=GROOVE),
-    "books_writer_lbl": Label(books, text="Writer:", bg="white"),
-    "books_writer_e": Entry(books, width=16, bd=2, relief=GROOVE),
+    "books_title_e": Entry(books, width=16, bd=2, relief=GROOVE, textvariable=book_title),
+    "books_author_lbl": Label(books, text="Author:", bg="white"),
+    "books_author_e": Entry(books, width=16, bd=2, relief=GROOVE, textvariable=book_author),
     "books_page_lbl": Label(books, text="Pages:", bg="white"),
-    "books_page_spn": Spinbox(books, from_=1, to=1000, width=14, bd=2, relief=GROOVE),
-    "books_add_book_btn": Button(books, text="Add Book", relief=FLAT),
+    "books_page_spn": Spinbox(books, from_=1, to=1000, width=14, bd=2, relief=GROOVE, textvariable=book_pages),
+    "books_add_book_btn": Button(books,
+                                 text="Add Book",
+                                 relief=FLAT,
+                                 command=lambda: items_data.add_book(book_title.get(), book_author.get(), book_pages.get())),
 }
+
+movie_title = StringVar()
+movie_director = StringVar()
+movie_length = IntVar()
 
 movie_widgets = {
     "movies_title_lbl": Label(movies, text="Title:", bg="white"),
-    "movies_title_e": Entry(movies, width=16, bd=2, relief=GROOVE),
+    "movies_title_e": Entry(movies, width=16, bd=2, relief=GROOVE, textvariable=movie_title),
     "movies_director_lbl": Label(movies, text="Director:", bg="white"),
-    "movies_director_e": Entry(movies, width=16, bd=2, relief=GROOVE),
+    "movies_director_e": Entry(movies, width=16, bd=2, relief=GROOVE, textvariable=movie_director),
     "movies_length_lbl": Label(movies, text="Length:", bg="white"),
-    "movies_length_spn": Spinbox(movies, from_=1, to=1000, width=14, bd=2, relief=GROOVE),
-    "movies_add_book_btn": Button(movies, text="Add Movie", relief=FLAT),
+    "movies_length_spn": Spinbox(movies, from_=1, to=1000, width=14, bd=2, relief=GROOVE, textvariable=movie_length),
+    "movies_add_book_btn": Button(movies,
+                                  text="Add Movie",
+                                  relief=FLAT,
+                                  command=lambda: items_data.add_movie(movie_title.get(), movie_director.get(), movie_length.get())),
 }
 
 for widget_collection in [book_widgets.values(), movie_widgets.values()]:
     for coords, widget in enumerate(widget_collection):
         widget.place(x=positions[coords][0], y=positions[coords][1])
-
-lst_box = Listbox(root, height=14, width=35, bd=2, relief=GROOVE)
-lst_box.place(x=245, y=35)
 
 filters = LabelFrame(root, text="Show", pady=5)
 filters.place(x=245, y=265)
